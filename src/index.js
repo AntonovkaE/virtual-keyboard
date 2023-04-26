e9d446036452660887886742b728976ddce75ae0
Elvira <sabirova.elvira.gam@gmail.com>
2023-04-26 14:08:53 +0500

    feat: do design for keyboard

--- src/index.js
+++ src/index.js
@@ -5,17 +5,40 @@ let fragment = new DocumentFragment();
 let keyboard = document.createElement('div')
 keyboard.classList.add('keyboard')
 
+function charIsLetter(char) {
+  if (typeof char !== 'string') {
+    return false;
+  }
+  return (/^[a-zA-Za-яА-Я]+$/.test(char) && char.length === 1);
+}
 
+function isWideKey(key, keyElement) {
+  switch (key) {
+    case 'tab':
+    case 'shift':
+    case 'enter':
+    case 'backspace':
+      keyElement.classList.add('key_wide');
+      break;
+    case 'space':
+      keyElement.classList.add('key_space');
+      break;
+  }
+}
 
-keysEn.forEach(key => {
-  const keyElement = document.createElement("button");
-  const insertLineBreak = ["backspace", "]", "enter", "\\", "shift"].indexOf(key) !== -1;
-
-  // Add attributes/classes
-  keyElement.setAttribute("type", "button");
-  keyElement.classList.add("keyboard__key");
-  keyboard.appendChild(keyElement);
-})
+for (let rowNumb in keysEn) {
+  let row = document.createElement('div')
+  row.classList.add('keyboard__row')
+  keysEn[rowNumb].forEach(key => {
+    const keyElement = document.createElement("button");
+    isWideKey(key, keyElement)
+    keyElement.textContent = key
+    keyElement.setAttribute("type", "button");
+    keyElement.classList.add("keyboard__key");
+    row.appendChild(keyElement);
+  })
+  keyboard.appendChild(row)
+}
 
 fragment.appendChild(keyboard)
 document.querySelector('body').appendChild(fragment)
--- src/utils/keys.js
+++ src/utils/keys.js
@@ -1,12 +1,12 @@
-const keysEn = [
-  "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "backspace",
-  "tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]",
-  "caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "\\", "enter",
-  "shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?", "shift",
-  "fn", "control", "option", "command", "space", "command", "option", "←","↑", "↓", "→",
-];
-const keysRu = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
-
-]
+const keysEn = {
+  row1: ['§', "1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "-", "=", "backspace"],
+  row2: ["tab", "q", "w", "e", "r", "t", "y", "u", "i", "o", "p", "[", "]"],
+  row3: ["caps", "a", "s", "d", "f", "g", "h", "j", "k", "l", ";", "'", "\\", "enter"],
+  row4: ["shift", "z", "x", "c", "v", "b", "n", "m", ",", ".", "?", "shift"],
+  row5: ["fn", "control", "option", "cmd", "space", "cmd", "option", "←", "↑", "↓", "→"],
+};
+// const keysRu = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", "backspace",
+//
+// ]
 
 export {keysEn}
